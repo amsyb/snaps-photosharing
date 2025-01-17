e5131c600fb4c6e9f8d0acc5a246548b76853220
annasybingco <sybingcoanna@gmail.com>
2025-01-17 12:57:48 -0500

    add styling to hero and images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <>
     <div className="hero">
-    <p className="body">Our mission</p>
-    <p className="title">Provide photographers a space to share photos of the neighborhoods they cherish, expressed in thier unique style</p>
+    <p className="body">Our mission:</p>
+    <p className="title">Provide photographers a space to share photos of the neighborhoods they cherish, <i>expressed in thier unique style</i></p>
     </div>
     </>
   )
