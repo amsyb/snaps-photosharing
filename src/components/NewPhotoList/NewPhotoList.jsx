fd552b1141cd320361edc832b10ec2871e147c0c
annasybingco <sybingcoanna@gmail.com>
2025-01-23 18:56:26 -0500

    update main page to display photos

--- src/components/NewPhotoList/NewPhotoList.jsx
+++ src/components/NewPhotoList/NewPhotoList.jsx
@@ -1,30 +1,42 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const API_KEY = "6e44d21f-2f0a-42a6-bd89-1adacd11a126"
+const API_KEY ="6e44d21f-2f0a-42a6-bd89-1adacd11a126";
 
-function NewPhotoList() {
-    const [photo, setPhotos] = useState([])
+const NewPhotoList = () => {
+  const [photos, setPhotos] = useState(null);
 
-      const getPhotos = async () => {
-        try {
-          const response = await axios.get(
-            `https://unit-3-project-c5faaab51857.herokuapp.com/photos?api_key=${API_KEY}`
-          );
-          setPhotos(response.data.photo);
-        } catch (error) {
-          console.log("error", error)
-        }
-      };
+  const getPhotos = async () => {
+    try {
+      const response = await axios.get(
+        `https://unit-3-project-c5faaab51857.herokuapp.com/photos?api_key=${API_KEY}`
+      );
+      console.log("API Response:", response.data)
+      setPhotos(response.data);
+    } catch (error) {
+      console.log("Error fetching movies", error);
+    }
+  };
+ 
 
-      useEffect(() => {
-        console.log("Photos state updated:", getPhotos);
-      }, [photo]);
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="photos">
+    {photos &&
+      photos.map((photo) => (
+        <article key={photo.id} className="photo-card__container">
+          <img
+            className="photos__img"
+            src={photo.photo}
+            alt={`Photo ${photo.photoDescription}`}
+          />
+        </article>
+      ))}
+  </section>
+  );
+};
 
-export default NewPhotoList
+export default NewPhotoList;
