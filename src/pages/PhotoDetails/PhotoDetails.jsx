a10d8c18e66664af0cfdead38e9b2cda2cf7f346
annasybingco <sybingcoanna@gmail.com>
2025-02-01 18:17:25 -0500

    remove commented out code

--- src/pages/PhotoDetails/PhotoDetails.jsx
+++ src/pages/PhotoDetails/PhotoDetails.jsx
@@ -1,35 +1,17 @@
-import SecondaryNav from "../../components/SecondaryNav/SecondaryNav"
-import PhotoId from "../../components/PhotoId/PhotoId"
-import PhotoDetails1 from "../../components/PhotoDetails1/PhotoDetails1"
-import PhotoComments from "../../components/PhotoComments/PhotoComments"
-// import axios from 'axios'
-// import { useState, useEffect } from 'react';
+import SecondaryNav from "../../components/SecondaryNav/SecondaryNav";
+import PhotoId from "../../components/PhotoId/PhotoId";
+import PhotoDetails1 from "../../components/PhotoDetails1/PhotoDetails1";
+import PhotoComments from "../../components/PhotoComments/PhotoComments";
 
 function PhotoDetails() {
-  // const [photo, setPhoto] = useState([])
-
-  // const getPhotos = async () => {
-  //   try {
-  //     const photosResponse = await axios.get (
-  //       `http://localhost:8888/api/photos/endpoint2`
-  //     );
-  //     setPhoto(photosResponse.data);
-  //   } catch (error) {
-  //     console.error("error", error)
-  //   }
-  //   }
-
-  // useEffect(() => {
-  //   getPhotos(photo)
-  //     })
   return (
     <>
-    <SecondaryNav />
+      <SecondaryNav />
       <PhotoId />
       <PhotoComments />
       <PhotoDetails1 />
-      </>
-  )
+    </>
+  );
 }
 
-export default PhotoDetails
+export default PhotoDetails;
