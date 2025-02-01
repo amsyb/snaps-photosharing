b422a35ad14090ee5782b893373aa3885b7ba203
annasybingco <sybingcoanna@gmail.com>
2025-01-31 19:39:03 -0500

    update homepage file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from './pages/Homepage/Homepage';
+import Homepage from './pages/Homepage/homepage';
 import PhotoDetails from "./pages/PhotoDetails/PhotoDetails";
 import Footer from "./components/Footer/Footer";
 
